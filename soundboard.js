032bf2c6133e379bcc4547ce896683e78e91f1e2
a-Pwjxj <wangste6@msu.edu>
2021-12-03 15:29:58 -0500

    js

--- soundboard.js
+++ soundboard.js
@@ -1,4 +1,4 @@
 function playSpawn() {
-    var spawn = new Audio("voicelines/keeper_of_the_light_keep_spawn_01.mp3.mpeg.mp3");
+    var spawn = new Audio('voicelines/Vo_keeper_of_the_light_keep_spawn_01.mp3.mpeg.mp3');
     spawn.play();
 }
