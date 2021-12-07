f019ae55aa40383b822b74968dc5c9b7f16bb56d
a-Pwjxj <wangste6@msu.edu>
2021-12-06 23:41:59 -0500

    lore button

--- soundboard.js
+++ soundboard.js
@@ -2,3 +2,8 @@ function playSpawn() {
     var spawn = new Audio('voicelines/Vo_keeper_of_the_light_keep_spawn_01.mp3.mpeg.mp3');
     spawn.play();
 }
+
+function playRare() {
+    var spawn = new Audio('voicelines/Vo_keeper_of_the_light_keep_rare_04.mp3.mpeg.mp3');
+    spawn.play();
+}
